e9579316d4e03b583f40c43d519faff18f9a6d5c
Cysmi <cnb10100@gmail.com>
2022-02-14 19:35:31 -0500

    added seeds to db

--- server.js
+++ server.js
@@ -1,28 +1,24 @@
 const path = require('path');
 const express = require('express');
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const sequelize = require('./config/connection');
 
-// turn on public
-app.use(express.static(path.join(__dirname, 'public')));
-
-// turn on routes
-app.use(routes);
-
-
-// turn on connection to db and server
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
-});
-
-const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers/'));
+
+// turn on connection to db and server
+sequelize.sync({ force: false}).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
