9411633f4492374417944eebd4fe3753fcbec8ea
Cysmi <cnb10100@gmail.com>
2022-02-16 23:05:28 -0500

    finished single-post page functionality

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -36,7 +36,10 @@ router.get("/", (req, res) => {
       // loop over all sequelize objects
       const posts = dbPostData.map((post) => post.get({ plain: true }));
       // pass a single post object into the homepage template
-      res.render("homepage", { posts });
+      res.render('homepage', {
+        posts,
+        loggedIn: req.session.loggedIn
+      });
     })
     .catch((err) => {
       console.log(err);
@@ -95,7 +98,10 @@ router.get("/post/:id", (req, res) => {
       const post = dbPostData.get({ plain: true });
 
       // pass data to template
-      res.render("single-post", { post });
+      res.render('single-post', {
+        post,
+        loggedIn: req.session.loggedIn
+      });
     })
     .catch((err) => {
       console.log(err);
